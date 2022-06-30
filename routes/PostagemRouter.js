7a9ec103e32e2106448bbcb4868a9049618823f9
GuiNo12 <guilherme.nogueiracoelho@live.com>
2022-06-30 11:33:24 -0300

    Adição rotas para like e deslike

--- routes/PostagemRouter.js
+++ routes/PostagemRouter.js
@@ -7,9 +7,9 @@ require('../models/Postagem');
 
 const Postagem = mongoose.model("postagens");
 
-router.get("/postagens",async (req,res) => {
+router.get("/postagens", async (req, res) => {
     let response;
-    
+
     await Postagem.find({}).then((postagens) => {
         response = postagens;
     }).catch((erro) => {
@@ -19,13 +19,13 @@ router.get("/postagens",async (req,res) => {
     res.json(response);
 });
 
-router.post("/postagens",verificaJWT,async (req,res) => {
+router.post("/postagens", verificaJWT, async (req, res) => {
     let response;
-    let {username,mensagem} = req.body;
+    let { username, mensagem } = req.body;
 
     let novaPostagem = {
-        username:username,
-        mensagem:mensagem
+        username: username,
+        mensagem: mensagem
     }
 
 
@@ -33,36 +33,96 @@ router.post("/postagens",verificaJWT,async (req,res) => {
         response = "Postagem adicionada com sucesso!";
 
     }).catch((erro) => {
-        
-        response = {erro:erro.message};
+
+        response = { erro: erro.message };
     });
 
     res.json(response);
 });
 
-router.post("/postagens/insereComentario",verificaJWT,async (req,res) => {
+router.post("/postagens/insereComentario", verificaJWT, async (req, res) => {
     let response;
-    let {username,mensagem,idPost} = req.body;
+    let { username, mensagem, idPost } = req.body;
 
     let novoComentario = {
-        username:username,
-        mensagem:mensagem,
+        username: username,
+        mensagem: mensagem,
         data: new Date()
     }
 
-    if(mensagem == null || mensagem == ""){
-        response = {erro:"Comentário não pode ser vazio!"}
-    }else{
-        await Postagem.findOne({_id:idPost}).then(async (postagem) => {
+    if (mensagem == null || mensagem == "") {
+        response = { erro: "Comentário não pode ser vazio!" }
+    } else {
+        await Postagem.findOne({ _id: idPost }).then(async (postagem) => {
             postagem.comentarios.push(novoComentario);
 
             await postagem.save().then(() => {
-                response = {"Mensagem":"Comentario adicionado com sucesso!"}; 
+                response = { "mensagem": "Comentario adicionado com sucesso!" };
             });
         }).catch((erro) => {
-            response = {erro:erro.message};
+            response = { erro: erro.message };
         });
     }
+
+    res.json(response);
+});
+
+router.post("/postagens/insereLike", verificaJWT, async (req, res) => {
+    let response;
+    let { username, idPost } = req.body;
+
+    let novoLike = {
+        username: username
+    }
+
+    await Postagem.findOne({ _id: idPost }).then(async (postagem) => {
+        if (postagem.deslikes.find(deslike => deslike.username == username)) {
+            postagem.deslikes.splice(postagem.deslikes.findIndex(deslike => deslike.username == username), 1);
+        }
+
+        if (postagem.likes.find(like => like.username == username)) {
+            throw new Error("Ação já Realizada!");
+        }
+        else {
+            postagem.likes.push(novoLike);
+        }
+
+        await postagem.save().then(() => {
+            response = { "mensagem": "Like adicionado com sucesso!" };
+        });
+    }).catch((erro) => {
+        response = { erro: erro.message };
+    });
+
+    res.json(response);
+});
+
+router.post("/postagens/insereDeslike", verificaJWT, async (req, res) => {
+    let response;
+    let { username, idPost } = req.body;
+
+    let novoDeslike = {
+        username: username
+    }
+
+    await Postagem.findOne({ _id: idPost }).then(async (postagem) => {
+        if (postagem.likes.find(like => like.username == username)) {
+            postagem.likes.splice(postagem.likes.findIndex(like => like.username == username), 1);
+        }
+
+        if (postagem.deslikes.find(deslike => deslike.username == username)) {
+            throw new Error("Ação já Realizada!");
+        }
+        else {
+            postagem.deslikes.push(novoDeslike);
+        }
+
+        await postagem.save().then(() => {
+            response = { "mensagem": "Deslike adicionado com sucesso!" };
+        });
+    }).catch((erro) => {
+        response = { erro: erro.message };
+    });
 
     res.json(response);
 });
